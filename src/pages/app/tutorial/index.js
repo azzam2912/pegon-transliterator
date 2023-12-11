9a75e61818788201c7a19716400f2eb43ff3fa41
azzam2912 <azzam.gamers12@gmail.com>
2023-12-11 21:16:44 +0700

    Delete unnecessary module and page, only pegon-latin transliteration thingy left

--- src/pages/app/tutorial/index.js
+++
@@ -1,6 +0,0 @@
-import {Page} from "src/componentPage/Page";
-import TutorialPage from "src/componentPage/TutorialPage/";
-
-export default function Tutorial() {
-  return <Page pageComponent={TutorialPage} />;
-}
